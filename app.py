ca14672f5d9f282613236598e524af31f27f2011
vuyanzi <vuyanziglorious@gmail.com>
2019-05-10 14:10:14 +0300

    first python commit

---
+++ app.py
@@ -0,0 +1,10 @@
+
+name = input ('What is your name? ')
+print ('Hi '+ name)
+colour = input ('What is your favourite colour ' + name + '')
+print (name + ' likes ' + colour)
+year_of_birth = input (name + ' Please enter your year of birth ' )
+age = 2019 - int(year_of_birth)
+g = 'Your current age is '
+v = age
+print (g + str(v))
