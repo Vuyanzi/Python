2b5851e7d039441d4b5f286cc3bb561e14171920
vuyanzi <vuyanziglorious@gmail.com>
2019-05-10 16:20:40 +0300

    strings

--- next.py
+++ next.py
@@ -3,3 +3,18 @@ kilograms = int(pound_weight) * 0.45
 g = 'Your weight in kilograms is '
 v = kilograms
 print(g + str(v))
+course = "python's course for beginners"
+print(course)
+course = "python course for 'beginners'"
+print(course[0])
+print(course[-3])
+print(course[0:6])
+print(course[0:])
+print(course[1:])
+print(course[:6])
+print(course[:])
+message = '''Hey everyone 
+its never too late to start 
+sooooo take the first step always and stick to it'''
+print(message)
+input("Does it ever get easy")
