df60da077a999a0f2106924d23224ad1bca9abca
vuyanzi <vuyanziglorious@gmail.com>
2019-05-10 15:51:07 +0300

    convertion of kilograms to pounds

--- next.py
+++ next.py
@@ -1,1 +1,5 @@
-print ('Hellow world')
+pound_weight = input ('What is your weight in pounds ')
+kilograms = int(pound_weight) * 0.45
+g = 'Your weight in kilograms is '
+v = kilograms
+print(g + str(v))
