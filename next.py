8e62436dd8ed0b4e6d738d12c08173923bd1efbe
vuyanzi <vuyanziglorious@gmail.com>
2019-05-11 04:58:53 +0300

    Continuation

--- next.py
+++ next.py
@@ -17,4 +17,40 @@ message = '''Hey everyone
 its never too late to start 
 sooooo take the first step always and stick to it'''
 print(message)
-input("Does it ever get easy")
+# conditions
+answer= input("Does it ever get easy")
+while answer == 'No':
+    print("That's how life is")
+if answer == 'Yes':
+    print("Good for you, not everyone gets it easy in life")
+else:
+    print("Don't worry, you'll figure it out soon")
+print("But we all keep going either way")
+'''string formatting
+%s - String (or any object with a string representation, like numbers)
+%d - Integers
+%f - Floating point numbers
+%.<number of digits>f - Floating point numbers with a fixed amount of digits to the right of the dot.
+%x/%X - Integers in hex representation (lowercase/uppercase)
+You will need to write a format string which prints out the data using the following syntax: 
+Hello John Doe. Your current balance is $53.44.'''
+fname = 'Glorious'
+sname = 'Vuyanzi'
+data = 34.5
+print('Hello %s %s.Your current balance is $%.2f' % (fname, sname, data))
+# given solution
+data = ("John", "Doe", 53.44)
+format_string = "Hello %s %s. Your current balance is $%s."
+print(format_string % data)
+root = "sample"
+here = "output"
+path = f"{root}/{here}"
+print(path)
+# string operations
+course = 'Python for beginners'
+print(course.title())
+print(course.upper())
+print(course.lower())
+print(course.find('beginners'))
+print(course.replace('beginners', 'everyone'))
+print('Python' in course)
